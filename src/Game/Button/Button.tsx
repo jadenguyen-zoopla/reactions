ce8c0ff4199ebb2a56de87d89aa6ce15821baba3
Jade <jade.nguyen@zoopla.co.uk>
2020-08-06 08:06:26 +0100

    added functionality to buttons

--- src/Game/Button/Button.tsx
+++ src/Game/Button/Button.tsx
@@ -1,4 +1,4 @@
-﻿import React, {FunctionComponent} from "react";
+﻿import React, { FunctionComponent } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
@@ -6,19 +6,30 @@ interface ButtonProps {
     x: number;
     y: number;
     activeButtonId: number;
+    setActiveButtonId: (id: number) => void; //this is a funtion that takes in number as a parameter but returns nothing
 }
 
-export const Button: FunctionComponent<ButtonProps> = ({id, x, y, activeButtonId}) => {
+export const Button: FunctionComponent<ButtonProps> = ({ id, x, y, activeButtonId, setActiveButtonId }) => {
 
     const isActive = id === activeButtonId;
-    const getClassName = () : string => {
+
+    const getClassName = (): string => {
         if (isActive) {
             return `${styles.button} ${styles.active}`
         }
         return styles.button;
     }
 
+    const buttonClicked = (): void => {
+        if(isActive) {
+            const randomButton = Math.floor(Math.random() * 8);
+            setActiveButtonId(randomButton);
+        } 
+    }
+
     return (
-        <circle className={getClassName()} cx={x} cy={y} r={100}/>
+        <circle onClick={buttonClicked} className={getClassName()} cx={x} cy={y} r={100} />
     );
 };
+
+
